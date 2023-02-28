e55595aa1b880989015f983e7098c93541e02498
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-02-28 10:20:35 +0100

    Create a while loop to collect user command

--- textEditor.c
+++ textEditor.c
@@ -8,5 +8,11 @@ int main() {
   char filename[50], command[10], text[1000];
   FILE *fp;
 
+     // Prompt user to enter command
+   while (1) {
+    printf("Enter a command (create, retrieve, update, delete, exit): ");
+    scanf("%s", command);
+   }
+
   return 0;
 }

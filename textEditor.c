4b506cd58a196c2b32531ba8941bd4bb985da4fa
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-02-28 10:13:14 +0100

    Declear our variables

---
+++ textEditor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // Declear variables 
+    // filename <=50 characters, command <=10 characters, text <=1000 characters
+  char filename[50], command[10], text[1000];
+  FILE *fp;
+
+  return 0;
+}

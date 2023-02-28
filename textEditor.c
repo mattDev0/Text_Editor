2ea5d2ce381c3ce5ad7e79f3258039896e6744cc
mattDev0 <abraham.odunayo.191009@fuoye.edu.ng>
2023-02-28 10:29:07 +0100

    create: implement the create feature

--- textEditor.c
+++ textEditor.c
@@ -12,6 +12,26 @@ int main() {
    while (1) {
     printf("Enter a command (create, retrieve, update, delete, exit): ");
     scanf("%s", command);
+
+     //creates a file when user enters "create"
+    if (strcmp(command, "create") == 0) {
+      printf("Enter the name of the file to create: ");
+      scanf("%s", filename);
+
+      fp = fopen(filename, "w");
+      if (fp == NULL) {
+        printf("Error: could not create file.\n");
+        continue;
+      }
+
+      printf("Enter text to write to file (max 1000 characters):\n");
+      scanf(" %[^\n]s", text);
+      fputs(text, fp);
+
+      fclose(fp);
+    }
+
+
    }
 
   return 0;
